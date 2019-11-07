1e5ed03af26f219de4e629d5bf063a0b4390f17c
joebadmu <joebadmus@gmail.com>
2019-11-07 05:43:46 +0000

    Added app-sever initial files

--- app-server.tf
+++ app-server.tf
@@ -1,25 +1,25 @@
-data "template_file" "jenkins_data" {
-  template = "${file("template/jenkins-data.tpl")}"
+data "template_file" "app_data" {
+  template = "${file("template/appserver-data.tpl")}"
 }
 
-resource "aws_instance" "JenkinsBox" {
+resource "aws_instance" "AppBox" {
   ami                         = "${var.ami_id}"
   instance_type               = "${var.instance_type}"
   associate_public_ip_address = true
   ebs_optimized               = false
   key_name                    = "${var.key_name}"
-  user_data                   = "${data.template_file.jenkins_data.rendered}"
+  user_data                   = "${data.template_file.app_data.rendered}"
   #subnet_id                   = "${aws_subnet.public[0]}"
   subnet_id              = "${element(aws_subnet.public.*.id, 0)}"
-  vpc_security_group_ids = ["${aws_security_group.jenkins_allow.id}"]
+  vpc_security_group_ids = ["${aws_security_group.app_allow.id}"]
 
   tags = {
-    Name = "JenkinsBox"
+    Name = "AppBox"
   }
 }
 
-resource "aws_security_group" "jenkins_allow" {
-  name        = "allow_all"
+resource "aws_security_group" "app_allow" {
+  name        = "allow_app"
   description = "Allow all inbound traffic"
   vpc_id      = "${aws_vpc.vpc.id}"
 
@@ -30,20 +30,13 @@ resource "aws_security_group" "jenkins_allow" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  # This should not be accessible from ouyr
+  # ingress {
+  #   from_port   = 8080
+  #   to_port     = 8080
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   egress {
     from_port   = 0
@@ -52,7 +45,7 @@ resource "aws_security_group" "jenkins_allow" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "JenkinsBox SG"
+    Name = "AppBox SG"
   }
 }
 
---
+++ vpc.tf
@@ -0,0 +1,76 @@
+# data "aws_availability_zones" "available" {}
+
+# resource "aws_vpc" "vpc" {
+#   cidr_block           = "${var.vpc_cidr}"
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+
+#   tags = {
+#     name        = "main_${var.environment}-vpc"
+#     environment = "${var.environment}"
+#     managed_by  = "${var.managed_by}"
+#   }
+# }
+
+# resource "aws_internet_gateway" "ig" {
+#   vpc_id = "${aws_vpc.vpc.id}"
+
+#   tags = {
+#     name        = "main_-${var.environment}-ig"
+#     environment = "${var.environment}"
+#     managed_by  = "${var.managed_by}"
+#   }
+# }
+
+# resource "aws_subnet" "public" {
+#   count                   = "${var.az_count}"
+#   vpc_id                  = "${aws_vpc.vpc.id}"
+#   cidr_block              = "${cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)}"
+#   availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+#   map_public_ip_on_launch = false
+
+#   tags = {
+#     name        = "${var.app_name}-${var.environment}-public-subnet-${count.index}"
+#     environment = "${var.environment}"
+#     managed_by  = "${var.managed_by}"
+#   }
+# }
+
+# resource "aws_route_table" "public" {
+#   count  = "${var.az_count}"
+#   vpc_id = "${aws_vpc.vpc.id}"
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = "${aws_internet_gateway.ig.id}"
+#   }
+
+#   tags = {
+#     name        = "${var.app_name}-${var.environment}-public-routes"
+#     environment = "${var.environment}"
+#     managed_by  = "${var.managed_by}"
+#   }
+# }
+
+# resource "aws_route_table_association" "public" {
+#   count          = "${var.az_count}"
+#   subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
+#   route_table_id = "${element(aws_route_table.public.*.id, count.index)}"
+# }
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config {
+    bucket = "joeb-s3-backend-${var.environment}"
+    name   = "hashicorp/vpc-prod"
+    key    = "dev/terraform.tfstate"
+    region = "${var.region}"
+  }
+}
+
+# resource "aws_instance" "foo" {
+#   # ...
+#   subnet_id = "${data.terraform_remote_state.vpc.subnet_id}"
+# }" "name" {
+
+# }
